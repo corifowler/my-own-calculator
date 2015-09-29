80a4335c7b8aa3ee28d0296288dd1a1eb6076380
Cori Fowler <fowler.cori@gmail.com>
2015-09-29 16:16:04 -0400

    Added variables and ids for each number in calculator

---
+++ main.js
@@ -0,0 +1,31 @@
+// Operators
+
+var multiplication = function () {
+
+};
+
+var division = function () {
+
+};
+
+var addition = function () {
+
+};
+
+var subtraction = function () {
+
+};
+
+// Variables
+
+var numZero = document.querySelector('#numz');
+var numOne = document.querySelector('#num1');
+var numTwo = document.querySelector('#num2');
+var numThree = document.querySelector('#num3');
+var numFour = document.querySelector('#num4');
+var numFive = document.querySelector('#num5');
+var numSix = document.querySelector('#num6');
+var numSeven = document.querySelector('#num7');
+var numEight = document.querySelector('#num8');
+var numNine = document.querySelector('num9');
+
